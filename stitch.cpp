0f97e9698dc0265beb5c3214163c2a9e008fb979
divyabudihal <divyabudihal@gmail.com>
2018-03-04 01:08:44 -0800

    trying different approach

--- stitch.cpp
+++ stitch.cpp
@@ -543,15 +543,18 @@
 //     cout << vidOut.isOpened() << endl;
 //     cout << frame_width << " X " << frame_height << endl;
 //     // cout << ((record[0]).size() << endl;
+//     int i = 0;
 //     while(1)
 //     { 
-//     Mat frame; 
+//     Mat frame = record[i];
           
 //     // Capture frame-by-frame 
-//     if (record.empty())
+//     // if (record.empty())
+//     //     break;
+//     if (i >= record.size())
 //         break;
 //     // frame = record.front();
-//     (record[0]).copyTo(frame);
+//    // (record[0]).copyTo(frame);
     
 //     // If the frame is empty, break immediately
 //     if (frame.empty())
@@ -560,7 +563,7 @@
 //     // cout << frame << endl;
 //     // Write the frame into the file 'outcpp.avi'
 //     // cout << (record[0]) << endl;
-//     vidOut.write(frame);
+//     vidOut.write(record[i]);
 //     // vidOut.operator << (frame);
 //     // Display the resulting frame    
 //     imshow( outputWindow, frame );
@@ -570,6 +573,7 @@
 //     if( c == 27 ) 
 //         break;
 //     }
+//     i++;
 // }
 
 // int main(int, char**)
@@ -701,106 +705,298 @@
 //     return 0;
 // }
 
+// // #include "opencv2/opencv.hpp"
+// // #include <iostream>
+// // #include <stdio.h>
+// // #include <thread>
+// // #include <string>
+// // #include <queue>
+
+// // using namespace cv;
+// // using namespace std;
+
+// // void processVid(VideoCapture& vid, queue<Mat>& record, string vidInput)
+// // {
+    
+// //     while(1)
+// //     {
+// //         Mat frame;
+// //         Mat enqueue;
+// //         vid >> frame; // get a new frame from video
+// //         if (frame.empty())
+// //             break;
+// //         bitwise_not (frame, frame);
+// //         cvtColor(frame, frame, CV_BGR2GRAY);
+// //         // imshow(outputWindow, frame);
+// //         // record.push(myMat()); // create a new empty mat
+// //         // frame.copyTo(record.front().frame);  // take a full copy
+// //         // record.front().inputFile = vidInput;
+// //         // enqueue.inputFile = vidInput;
+// //         // frame.copyTo(enqueue.frame);
+// //         frame.copyTo(enqueue);
+// //         record.push(enqueue);
+        
+// //         cout << "Frame Size: " << (record.front()).size() << endl;
+// //         cout << vidInput << " Queue Size: " << record.size() << endl;
+// //         // cout << record.front().frame << endl;
+// //         // cout << record.front().inputFile << endl;
+// //         cout << "Reading from " << vidInput << endl;
+// //         if(waitKey(30) >= 0) break;
+// //     }
+// //     return;
+// // }
+
+
+
+// void outputVid (VideoCapture& vid, queue<Mat>& record, string outputWindow, string outputFile)
+// {
+//     // Default resolution of the frame is obtained.The default resolution is system dependent. 
+//     int frame_width = vid.get(CV_CAP_PROP_FRAME_WIDTH); 
+//     int frame_height = vid.get(CV_CAP_PROP_FRAME_HEIGHT); 
+
+//     // Define the codec and create VideoWriter object.The output is stored in 'outcpp.avi' file. 
+//     VideoWriter vidOut(outputFile,CV_FOURCC('M','J','P','G'),10, Size(frame_width,frame_height)); 
+//     // VideoWriter vidOut(outputFile,CV_FOURCC('D', 'I', 'V', 'X'),25, Size(frame_width,frame_height));
+//     cout << vidOut.isOpened() << endl;
+//     cout << frame_width << " X " << frame_height << endl;
+//     cout << (record.front()).size() << endl;
+//     while(1)
+//     { 
+//     Mat frame; 
+          
+//     // Capture frame-by-frame 
+//     if (record.empty())
+//         break;
+//     // frame = record.front();
+//     (record.front()).copyTo(frame);
+    
+//     // If the frame is empty, break immediately
+//     if (frame.empty())
+//         break;
+//     //bitwise_not (frame, frame); // invert frame
+//     // cout << frame << endl;
+//     // Write the frame into the file 'outcpp.avi'
+//     resize(frame,frame,Size(frame_width, frame_height));
+//     vidOut.write(frame);
+//     //  vidOut.write(record.front());
+//     // vidOut.operator << (frame);
+//     // Display the resulting frame    
+//     imshow( outputWindow, frame );
+//     record.pop();
+//     // Press  ESC on keyboard to  exit
+//     char c = (char)waitKey(30);
+//     if( c == 27 ) 
+//         break;
+//     }
+// }
+
+// int main(int, char**)
+// {
+//     // int frameHeight;
+//     // int frameWidth;
+//     // int frameRate;
+    
+//     string vid1Location = "./test5.mp4";
+//     string vid2Location = "./test6.mp4";
+
+//     string vid1Input = "test5.mp4";
+//     string vid2Input = "test6.mp4";
+
+//     string vid1Output = "work.avi";
+//     string vid2Output = "video2.avi";
+
+//     queue<Mat> vid1Queue;
+//     queue<Mat> vid2Queue;
+    
+//     VideoCapture vid1(vid1Location); // open the first test file
+//     if(!vid1.isOpened())  // check if we succeeded
+//     { 
+//         cout << "Video 1 File could not be successfully opened" << endl;
+//         return -1;
+//     }
+
+//     VideoCapture vid2(vid2Location); // open the second test file
+//     if(!vid2.isOpened())  // check if we succeeded
+//     { 
+//         cout << "Video 2 File could not be successfully opened" << endl;
+//         return -1;
+//     }
+
+//     cout << "Initializing thread to process Video 1..." << endl;
+//     // thread out1 (outputVid, ref(vid1), "Video 1 Output", vid1Output);     // spawn new thread that calls foo()
+//     thread out1 (processVid, ref(vid1), ref(vid1Queue), vid1Input);     // spawn new thread that calls foo()
+//     cout << "Initializing thread to process Video 2..." << endl;
+//     thread out2 (processVid, ref(vid2), ref(vid2Queue), vid2Input);  // spawn new thread that calls bar(0)
+    
+//     // cout << "Initializing thread to create output video...";
+//     // thread out3 (createVid, ref(vid2), ref(vid1Queue), ref(vid2Queue), vid1Output);
+   
+//     thread::id id1 = out1.get_id();
+//     thread::id id2 = out2.get_id();
+//     // thread::id id3 = out3.get_id();
+//     // cout << "Streaming of two videos now executing concurrently...\n";
+
+//     // // synchronize threads:
+//     if (out1.joinable())
+//     {
+//         cout << "Joining thread for Video 1, ID = " << id1 << endl;
+//         out1.join();                // pauses until first finishes
+//     }
+    
+   
+//     if (out2.joinable())
+//     {
+//         cout << "Joining thread for Video 2, ID = " << id2 << endl;
+//         out2.join();               // pauses until second finishes
+//     }
+
+//     outputVid(vid2, vid2Queue, "Hi", "doit.avi");
+//     // if (out3.joinable())
+//     // {
+//     //     cout << "Joining thread for Output Video, ID = " << id3 << endl;
+//     //     out3.join();               // pauses until second finishes
+//     // }
+
+//     destroyAllWindows();
+//     return 0;
+// }
+
 #include "opencv2/opencv.hpp"
 #include <iostream>
 #include <stdio.h>
 #include <thread>
 #include <string>
-#include <queue>
 
 using namespace cv;
 using namespace std;
 
-void processVid(VideoCapture& vid, queue<Mat>& record, string vidInput)
+void stream (VideoCapture& vid, string outputWindow)
 {
-    
+    namedWindow(outputWindow, WINDOW_NORMAL);
     while(1)
     {
         Mat frame;
-        Mat enqueue;
         vid >> frame; // get a new frame from video
         if (frame.empty())
             break;
-        bitwise_not (frame, frame);
-        cvtColor(frame, frame, CV_BGR2GRAY);
-        // imshow(outputWindow, frame);
-        // record.push(myMat()); // create a new empty mat
-        // frame.copyTo(record.front().frame);  // take a full copy
-        // record.front().inputFile = vidInput;
-        // enqueue.inputFile = vidInput;
-        // frame.copyTo(enqueue.frame);
-        frame.copyTo(enqueue);
-        record.push(enqueue);
-        
-        cout << "Frame Size: " << (record.front()).size() << endl;
-        cout << vidInput << " Queue Size: " << record.size() << endl;
-        // cout << record.front().frame << endl;
-        // cout << record.front().inputFile << endl;
-        cout << "Reading from " << vidInput << endl;
+        bitwise_not ( frame, frame);
+        imshow(outputWindow, frame);
         if(waitKey(30) >= 0) break;
     }
     return;
 }
 
-
-
-void outputVid (VideoCapture& vid, queue<Mat>& record, string outputWindow, string outputFile)
+void outputVid ( Mat& readerMat1, Mat& readerMat2, bool& reader1, bool& reader2, string outputWindow, string outputFile)
 {
-    // Default resolution of the frame is obtained.The default resolution is system dependent. 
-    int frame_width = vid.get(CV_CAP_PROP_FRAME_WIDTH); 
-    int frame_height = vid.get(CV_CAP_PROP_FRAME_HEIGHT); 
+    // // Default resolution of the frame is obtained.The default resolution is system dependent. 
+    // int frame_width = vid.get(CV_CAP_PROP_FRAME_WIDTH); 
+    // int frame_height = vid.get(CV_CAP_PROP_FRAME_HEIGHT); 
 
     // Define the codec and create VideoWriter object.The output is stored in 'outcpp.avi' file. 
-    VideoWriter vidOut(outputFile,CV_FOURCC('M','J','P','G'),10, Size(frame_width,frame_height)); 
-    // VideoWriter vidOut(outputFile,CV_FOURCC('m', 'p', '4', 'v'),10, Size(frame_width,frame_height));
-    cout << vidOut.isOpened() << endl;
-    cout << frame_width << " X " << frame_height << endl;
-    cout << (record.front()).size() << endl;
+    VideoWriter vidOut(outputFile,CV_FOURCC('M','J','P','G'),10, Size(1920,1080)); 
     while(1)
     { 
     Mat frame; 
-          
-    // Capture frame-by-frame 
-    if (record.empty())
-        break;
-    // frame = record.front();
-    (record.front()).copyTo(frame);
-    
+        
+    if (reader1 != false)
+    {
+        readerMat1.copyTo(frame);
+    }
+    else if (reader2 != false)
+    {
+        readerMat2.copyTo(frame);
+    }
+    else{
+        while (reader1 == 0 && reader2 == 0);
+    }
+
+
     // If the frame is empty, break immediately
     if (frame.empty())
         break;
-    //bitwise_not (frame, frame); // invert frame
-    // cout << frame << endl;
+    bitwise_not ( frame, frame); // invert frame
     // Write the frame into the file 'outcpp.avi'
     vidOut.write(frame);
-    // vidOut.operator << (frame);
+
     // Display the resulting frame    
     imshow( outputWindow, frame );
-    record.pop();
+
     // Press  ESC on keyboard to  exit
-    char c = (char)waitKey(30);
+    char c = (char)waitKey(1);
     if( c == 27 ) 
         break;
     }
 }
 
+
+void reader1 (VideoCapture& vid, Mat& readerMat1, bool& reader1)
+{
+
+    while(1)
+    { 
+    Mat frame; 
+        
+    // Capture frame-by-frame 
+    vid >> frame;
+
+    // If the frame is empty, break immediately
+    if (frame.empty())
+        break;
+    bitwise_not ( frame, frame); // invert frame
+    // Write the frame into the file 'outcpp.avi'
+    while (reader1 != false)
+    {
+        frame.copyTo(readerMat1);
+        reader1 = true;
+    }
+
+    char c = (char)waitKey(1);
+    if( c == 27 ) 
+        break;
+    }
+}
+
+void reader2 (VideoCapture& vid, Mat& readerMat2, bool& reader2)
+{
+
+    while(1)
+    { 
+    Mat frame; 
+        
+    // Capture frame-by-frame 
+    vid >> frame;
+
+    // If the frame is empty, break immediately
+    if (frame.empty())
+        break;
+    bitwise_not ( frame, frame); // invert frame
+    // Write the frame into the file 'outcpp.avi'
+    while (reader2 != false)
+    {
+        frame.copyTo(readerMat2);
+        reader2 = true;
+    }
+    
+    char c = (char)waitKey(1);
+    if( c == 27 ) 
+        break;
+    }
+}
+
+
 int main(int, char**)
 {
-    // int frameHeight;
-    // int frameWidth;
-    // int frameRate;
-    
-    string vid1Location = "./test5.mp4";
-    string vid2Location = "./test6.mp4";
+    string vid1Location = "./iris-tests/dynamic_test.mp4";
+    string vid2Location = "./iris-tests/field_trees.avi";
 
-    string vid1Input = "test5.mp4";
-    string vid2Input = "test6.mp4";
-
-    string vid1Output = "work.avi";
+    string vid1Output = "video1.avi";
     string vid2Output = "video2.avi";
 
-    queue<Mat> vid1Queue;
-    queue<Mat> vid2Queue;
+    Mat readerMat1;
+    Mat readerMat2;
+
+    bool reader1Flag;
+    bool reader2Flag;
     
     VideoCapture vid1(vid1Location); // open the first test file
     if(!vid1.isOpened())  // check if we succeeded
@@ -816,41 +1012,38 @@ int main(int, char**)
         return -1;
     }
 
-    cout << "Initializing thread to process Video 1..." << endl;
-    // thread out1 (outputVid, ref(vid1), "Video 1 Output", vid1Output);     // spawn new thread that calls foo()
-    thread out1 (processVid, ref(vid1), ref(vid1Queue), vid1Input);     // spawn new thread that calls foo()
-    cout << "Initializing thread to process Video 2..." << endl;
-    thread out2 (processVid, ref(vid2), ref(vid2Queue), vid2Input);  // spawn new thread that calls bar(0)
-    
-    // cout << "Initializing thread to create output video...";
-    // thread out3 (createVid, ref(vid2), ref(vid1Queue), ref(vid2Queue), vid1Output);
-   
-    thread::id id1 = out1.get_id();
-    thread::id id2 = out2.get_id();
+    cout << "Initializing thread for Video 1..." << endl;
+    thread out1 (reader1, ref(vid1), ref(readerMat1), ref(reader1Flag) );     // spawn new thread that calls foo()
+    cout << "Initializing thread for Video 2..." << endl;
+    thread out2 (reader2, ref(vid2), ref(readerMat2), ref(reader2Flag) );  // spawn new thread that calls bar(0)
+    cout << "Initializing thread for Output Video..." << endl;
+    thread out3 (outputVid, ref(readerMat1), ref(readerMat2), ref(reader1Flag), ref(reader2Flag), "Hi", vid2Output);
+
+    // thread::id id1 = out1.get_id();
+    // thread::id id2 = out2.get_id();
     // thread::id id3 = out3.get_id();
-    // cout << "Streaming of two videos now executing concurrently...\n";
+    // // cout << "Streaming of two videos now executing concurrently...\n";
 
-    // // synchronize threads:
-    if (out1.joinable())
-    {
-        cout << "Joining thread for Video 1, ID = " << id1 << endl;
-        out1.join();                // pauses until first finishes
-    }
+    // // // synchronize threads:
+    // if (out1.joinable())
+    // {
+    //     cout << "Joining thread for Video 1, ID = " << id1 << endl;
+    //     out1.join();                // pauses until first finishes
+    // }
     
-   
-    if (out2.joinable())
-    {
-        cout << "Joining thread for Video 2, ID = " << id2 << endl;
-        out2.join();               // pauses until second finishes
-    }
+    // if (out2.joinable())
+    // {
+    //     cout << "Joining thread for Video 2, ID = " << id2 << endl;
+    //     out2.join();               // pauses until second finishes
+    // }
 
-    outputVid(vid2, vid2Queue, "Hi", "do.avi");
     // if (out3.joinable())
     // {
     //     cout << "Joining thread for Output Video, ID = " << id3 << endl;
-    //     out3.join();               // pauses until second finishes
+    //     out3.join();
     // }
 
+    cout << "Done outputting to file" << endl;
     destroyAllWindows();
     return 0;
 }
