5db648d4520f276dc6758f77f62e1aae861dc8b7
divyabudihal <divyabudihal@gmail.com>
2018-03-04 07:19:43 -0800

    Merged readers, cleaned up some more

--- stitch.cpp
+++ stitch.cpp
@@ -870,8 +870,7 @@
 using namespace cv;
 using namespace std;
 
-bool reader1Done = false;
-bool reader2Done = false;
+#define OUTPUT_FPS 30
 
 bool readersDone = false;
 
@@ -891,7 +890,7 @@ void stream (VideoCapture& vid, string outputWindow)
     return;
 }
 
-void outputVid ( Mat& readerMat1, Mat& readerMat2, bool& reader1, bool& reader2, string outputWindow, string outputFile)
+void outputVid ( Mat& readerMat1, Mat& readerMat2, bool& reader1, bool& reader2, string outputWindow, string outputFile, int frameHeight, int frameWidth)
 {
     // // Default resolution of the frame is obtained.The default resolution is system dependent. 
     // int frame_width = vid.get(CV_CAP_PROP_FRAME_WIDTH); 
@@ -899,7 +898,7 @@ void outputVid ( Mat& readerMat1, Mat& readerMat2, bool& reader1, bool& reader2,
 
     // Define the codec and create VideoWriter object.The output is stored in 'outcpp.avi' file. 
     cout << "Starting to write to file..." << endl;
-    VideoWriter vidOut(outputFile,CV_FOURCC('M','J','P','G'),30, Size(1920,1080)); 
+    VideoWriter vidOut(outputFile,CV_FOURCC('M','J','P','G'),OUTPUT_FPS, Size(frameWidth,frameHeight)); 
     while(1)
     { 
     
@@ -1054,14 +1053,48 @@ void reader2 (VideoCapture& vid, Mat& readerMat2, bool& reader2)
     return;
 }
 
+void reader (VideoCapture& vid, Mat& readerMat, bool& reader, string vidInput)
+{
+    cout << vidInput << " Reader Active" << endl;
+    while(1)
+    { 
+
+    if (readersDone == true)
+    {
+        reader = false;
+        break; // stop reading because one of the videos is over
+    }
+   
+    while (reader != false && readersDone == false)
+    {
+       // wait until writer needs a new value
+    }
+    vid >> readerMat;
+    bitwise_not ( readerMat, readerMat); // invert readerMat
+
+    if (readerMat.empty())
+        break; // stop reading because the current video is over
+    // frame.copyTo(readerMat);
+    reader = true; // indicate that this reader's value is ready to be writter
+    char c = (char)waitKey(1);
+    if( c == 27 ) 
+        break;
+    
+    }
+    cout << "Reader is done capturing frames from " << vidInput << endl;
+    readersDone = true; // set readersDone flag to true because reader has finished 
+    return;
+}
+
 
 int main(int, char**)
 {
-    string vid1Location = "test5.mp4";
-    string vid2Location = "test6.mp4";
+    string vid1Input = "test5.mp4";
+    string vid2Input = "test6.mp4";
 
     string vid1Output = "video1.avi";
     string vid2Output = "video2.avi";
+
 
     int frameHeight;
     int frameWidth;
@@ -1072,14 +1105,14 @@ int main(int, char**)
     bool reader1Flag = false;
     bool reader2Flag = false;
     
-    VideoCapture vid1(vid1Location); // open the first test file
+    VideoCapture vid1(vid1Input); // open the first test file
     if(!vid1.isOpened())  // check if we succeeded
     { 
         cout << "Video 1 File could not be successfully opened" << endl;
         return -1;
     }
 
-    VideoCapture vid2(vid2Location); // open the second test file
+    VideoCapture vid2(vid2Input); // open the second test file
     if(!vid2.isOpened())  // check if we succeeded
     { 
         cout << "Video 2 File could not be successfully opened" << endl;
@@ -1111,12 +1144,15 @@ int main(int, char**)
         frameWidth = vid2.get(CV_CAP_PROP_FRAME_WIDTH);
     }
 
+    cout << frameHeight << endl;
+    cout << frameWidth << endl;
+    
     cout << "Initializing thread for Video 1..." << endl;
-    thread out1 (reader1, ref(vid1), ref(readerMat1), ref(reader1Flag) );     // spawn new thread that calls foo()
+    thread out1 (reader, ref(vid1), ref(readerMat1), ref(reader1Flag), vid1Input);     // spawn new thread that calls foo()
     cout << "Initializing thread for Video 2..." << endl;
-    thread out2 (reader2, ref(vid2), ref(readerMat2), ref(reader2Flag) );  // spawn new thread that calls bar(0)
+    thread out2 (reader, ref(vid2), ref(readerMat2), ref(reader2Flag), vid2Input);  // spawn new thread that calls bar(0)
     cout << "Initializing thread for Output Video..." << endl;
-    thread out3 (outputVid, ref(readerMat1), ref(readerMat2), ref(reader1Flag), ref(reader2Flag), "Hi", vid2Output);
+    thread out3 (outputVid, ref(readerMat1), ref(readerMat2), ref(reader1Flag), ref(reader2Flag), "Output Video", vid2Output, frameHeight, frameWidth);
 
     thread::id id1 = out1.get_id();
     thread::id id2 = out2.get_id();
